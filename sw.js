aa45e5a97766cfb73559488944fcb6802973a095
NavyanthKusampudi <n.kusampudi@mpie.de>
2026-04-28 12:16:48 +0200

    files for app

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "nonlinear-sound-analysis-v1";
+
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./phaseportrait-mobile.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
